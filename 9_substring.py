ae55ee7b83c3b494da93e72295cb8216c813cca6
anilvarma0502 <90962280+anilvarma0502@users.noreply.github.com>
2021-09-22 15:19:55 +0530

    Update 9_substring.py

--- 9_substring.py
+++ 9_substring.py
@@ -1,12 +1,9 @@
-# function to check if small string is 
-# there in big string
 def check(string, sub_str):
     if (string.find(sub_str) == -1):
         print("NO")
     else:
         print("YES")
             
-# driver code
 string = "python"
 sub_str ="py"
 check(string, sub_str)
